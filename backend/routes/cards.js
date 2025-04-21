0c2b478b2424f7421c2cb2ef920f30df196e9ab5
MarcosJVPR <perezmarcosjulio@gmail.com>
2025-04-21 22:56:46 +0000

    🪄 Log para rastrear entrada a /daily

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -3,15 +3,15 @@ import { getCardOfTheDay, createCard } from "../controllers/cardsController.js";
 
 const router = express.Router();
 
-// Ruta de prueba para saber si funciona
 router.get("/", (req, res) => {
-  res.send("🪄 Ruta /api/cards funcionando");
+  res.send("🪄 /api/cards está vivo");
 });
 
 router.get("/daily", (req, res, next) => {
   console.log("📥 Petición recibida en /daily");
   next();
 }, getCardOfTheDay);
+
 router.post("/", createCard);
 
 export default router;
