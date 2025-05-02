4ab199661cadefa29e3a87510567394a3c8cf047
MarcosJVPR <perezmarcosjulio@gmail.com>
2025-05-02 01:40:30 +0000

    cambios

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "frontend")));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
